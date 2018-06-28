8c17a977bacadbd4bee2d3223ed205560b666731
NeilShao <baodongshao@gmail.com>
2018-06-29 00:24:53 +0800

    Frist Commit

---
+++ Helpers/HttpServer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Networking.Sockets;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace File_Share.Helpers
+{
+    public class HttpServer : IDisposable
+    {
+        private const uint BufferSize = 8192;
+        private readonly StorageFolder LocalFolder;
+                     
+        private readonly StreamSocketListener listener;
+
+        public HttpServer(int port, StorageFolder Folder)
+        {
+            this.LocalFolder = Folder;
+
+            this.listener = new StreamSocketListener();
+            this.listener.ConnectionReceived += (s, e) => ProcessRequestAsync(e.Socket);
+            this.listener.BindServiceNameAsync(port.ToString());
+        }
+
+        public void Dispose()
+        {
+            this.listener.Dispose();
+        }
+
+        private async void ProcessRequestAsync(StreamSocket socket)
+        {
+            // this works for text only
+            StringBuilder request = new StringBuilder();
+            using (IInputStream input = socket.InputStream)
+            {
+                byte[] data = new byte[BufferSize];
+                IBuffer buffer = data.AsBuffer();
+                uint dataRead = BufferSize;
+                while (dataRead == BufferSize)
+                {
+                    await input.ReadAsync(buffer, BufferSize, InputStreamOptions.Partial);
+                    request.Append(Encoding.UTF8.GetString(data, 0, data.Length));
+                    dataRead = buffer.Length;
+                }
+            }
+
+            using (IOutputStream output = socket.OutputStream)
+            {
+                string requestMethod = request.ToString().Split('\n')[0];
+                string[] requestParts = requestMethod.Split(' ');
+
+                if (requestParts[0] == "GET")
+                    await WriteResponseAsync(requestParts[1], output);
+                else
+                    throw new InvalidDataException("HTTP method not supported: "
+                                                   + requestParts[0]);
+            }
+        }
+
+        private async Task WriteResponseAsync(string path, IOutputStream os)
+        {
+            using (Stream resp = os.AsStreamForWrite())
+            {
+                bool exists = true;
+                try
+                {
+                    // Look in the Data subdirectory of the app package
+                    string filePath = path.Replace("/", String.Empty);
+                    using (Stream fs = await LocalFolder.OpenStreamForReadAsync(filePath))
+                    {
+                        string header = String.Format("HTTP/1.1 200 OK\r\n" +
+                                        "Content-Length: {0}\r\n" +
+                                        "Connection: close\r\n\r\n",
+                                        fs.Length);
+                        byte[] headerArray = Encoding.UTF8.GetBytes(header);
+                        await resp.WriteAsync(headerArray, 0, headerArray.Length);
+                        await fs.CopyToAsync(resp);
+                    }
+                }
+                catch (FileNotFoundException)
+                {
+                    exists = false;
+                }
+
+                if (!exists)
+                {
+                    byte[] headerArray = Encoding.UTF8.GetBytes(
+                                          "HTTP/1.1 404 Not Found\r\n" +
+                                          "Content-Length:0\r\n" +
+                                          "Connection: close\r\n\r\n");
+                    await resp.WriteAsync(headerArray, 0, headerArray.Length);
+                }
+
+                await resp.FlushAsync();
+            }
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using File_Share.Helpers;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace File_Share
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void nvAll_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            
+        }
+
+        private async void More_Click(object sender, RoutedEventArgs e)
+        {
+            var folder = await PickFolder();
+            HttpServer http = new HttpServer(9999, folder);
+        }
+
+        private async Task<StorageFolder> PickFolder()
+        {
+            var folderPicker = new FolderPicker();
+            folderPicker.SuggestedStartLocation = PickerLocationId.Desktop;
+            folderPicker.FileTypeFilter.Add("*");
+
+            StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+            if (folder != null)
+            {
+                // Application now has read/write access to all contents in the picked folder
+                // (including other sub-folder contents)
+                Windows.Storage.AccessCache.StorageApplicationPermissions.
+                FutureAccessList.AddOrReplace("PickedFolderToken", folder);
+                return folder;
+            }
+
+            return null;
+        }
+    }
+}
